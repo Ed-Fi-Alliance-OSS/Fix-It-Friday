7f57e4fe540042c4558243493f890d06f9e8a3f9
Luis Richmond <lalrg@outlook.com>
2020-05-21 12:57:52 -0600

    -Add react-bootstrap -Remove unused files

--- fixitfriday.ui/src/App.js
+++ fixitfriday.ui/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+const Header = () => ( 
+  <Row className="justify-content-md-center">
+    <h1 className="header">Welcome To Fix it Fridays</h1> 
+  </Row>
+)
+
+const App = () => (
+  <Container className="p-3">
+    <Jumbotron>
+      <Header/>
+    </Jumbotron>
+  </Container>
+);
 
 export default App;
