32c167b25a7f8bf03e10698b3f56e85358069804
Kurt Johnson <kurt.johnson@headspring.com>
2020-05-28 16:39:46 -0500

    Remove JS files
    GraphqlClient, App and Index all caused problems. The hello word jumbotron from the JS shows on yarn start.
    
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

--- fixitfriday.ui/src/services/graphqlClient.ts
+++ fixitfriday.ui/src/services/graphqlClient.ts
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:52669/GraphQL',
+  uri: 'http://localhost:53923/GraphQL',
 });
 
 export default client;
