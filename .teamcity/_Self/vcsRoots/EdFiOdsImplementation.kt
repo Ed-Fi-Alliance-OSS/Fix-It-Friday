e55edafbe13a7c69628029a7d1e0d53d965a65e3
Stephen A. Fuqua <stephen@safnet.com>
2020-05-27 22:02:47 -0500

    Typo correction

--- .teamcity/_Self/vcsRoots/EdFiOdsImplementation.kt
+++ .teamcity/_Self/vcsRoots/EdFiOdsImplementation.kt
@@ -6,7 +6,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 object EdFiOdsImplementation : GitVcsRoot({
     name = "Ed-Fi-ODS-Implementation"
     url = "https://github.com/%github.organization%/Ed-Fi-ODS-Implementation.git"
-    branch = "%github.branch.default%"
+    branch = "%git.branch.default%"
     userNameStyle = GitVcsRoot.UserNameStyle.NAME
     checkoutSubmodules = GitVcsRoot.CheckoutSubmodules.IGNORE
     serverSideAutoCRLF = true
