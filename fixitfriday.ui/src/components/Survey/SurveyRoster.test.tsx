a095d698878134e57f7cf19e92be050c09c4fd58
jleiva-gap <jleiva@growthaccelerationpartners.com>
2020-06-05 09:23:21 -0600

    Update SurveyRoster.test.tsx

--- fixitfriday.ui/src/components/Survey/SurveyRoster.test.tsx
+++ fixitfriday.ui/src/components/Survey/SurveyRoster.test.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import { render,fireEvent  } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SurveyRosterModal from './SurveyRosterModal';
 import { SurveyClassType } from './types/SurveyClassType';
 
 test('renders Survey Modal', () => {
+  const sectionSurveys: Array<SurveyClassType> = [
+    {
+      surveyKey: '1',
+      surveyName: 'Test Survey',
+    },
+  ];
 
-  const sectionSurveys : Array<SurveyClassType> = [
-  {
-    surveyKey: '1',
-    surveyName: 'Test Survey'
-  }];
+  const { getByText, getByDisplayValue } = render(<SurveyRosterModal surveys={sectionSurveys} />);
+  fireEvent.click(getByText('Class Survey Results'));
 
-
-const { getByText, getByDisplayValue } = render(
-  <SurveyRosterModal
-    surveys={sectionSurveys}
-  />,
-);
-fireEvent.click(getByText('Class Survey Results'))
-
-const surveyHasSurveyKey = getByDisplayValue(sectionSurveys[0].surveyKey);
-const surveyHasSurveyName = getByText(sectionSurveys[0].surveyName);
+  const surveyHasSurveyKey = getByDisplayValue(sectionSurveys[0].surveyKey);
+  const surveyHasSurveyName = getByText(sectionSurveys[0].surveyName);
 
   expect(surveyHasSurveyKey).toBeTruthy();
   expect(surveyHasSurveyName).toBeTruthy();
