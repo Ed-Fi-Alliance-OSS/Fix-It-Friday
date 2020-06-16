3a2c860011c1539ffea6390c592ee685598b1a75
DavidJGapCR <djimenez@growthaccelerationpartners.com>
2020-06-16 14:07:54 -0600

    Adds project for database migrations.

---
+++ edfi.fif.database/migrations/sqls/20200616174352-initial-tables-down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS fif.StaffSectionAssociation;
+DROP TABLE IF EXISTS fif.Staff;
+DROP TABLE IF EXISTS fif.StudentSection;
+DROP TABLE IF EXISTS fif.Section;
+DROP TABLE IF EXISTS fif.StudentContact;
+DROP TABLE IF EXISTS fif.ContactPerson;
+DROP TABLE IF EXISTS fif.StudentSchool;
