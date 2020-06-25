797657a331c9ec12899940a7fb80bf72b4deab25
Kurt Johnson <kurt.johnson@headspring.com>
2020-06-25 15:42:43 -0500

    Use env file
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

--- EdFi.FiF.Etl/config/dbs.js
+++ EdFi.FiF.Etl/config/dbs.js
@@ -1,23 +1,28 @@
 var pgConfig = {
 	host: process.env.FIF_DBSERVER || '127.0.0.1',
-    user: process.env.FIF_USER || 'pguser',
+    user: process.env.FIF_USER || 'postgres',
 	password: process.env.FIF_PASSWORD || 'p@ssw0rd',
 	database: process.env.FIF_DBNAME || 'FixItFriday',
 	max: parseInt(process.env.FIF_MAX || 20),
-	idleTimeoutMillis:  parseInt(process.env.ODS_IDLETIMEOUTMILLIS || 30000),
-	connectionTimeoutMillis: parseInt(process.env.ODS_CONNECTIONTIMEOUTMILLIS || 2000),
+	idleTimeoutMillis:  parseInt(process.env.FIF_IDLETIMEOUTMILLIS || 30000),
+	connectionTimeoutMillis: parseInt(process.env.FIF_CONNECTIONTIMEOUTMILLIS || 2000),
 };
 
 var mssqlConfig = {
-    user: 'ods_user',
-    password: 'p@ssw0rd',
-    database: 'ods-with-amt',
-    server: 'localhost',
-    port: 49412,
-    requestTimeout: 300000,
-    options: {
-        enableArithAbort: true,
-        trustServerCertificate: true,
+    "server": process.env.ODS_SERVER || "127.0.0.1",
+    "authentication": {
+        "type": "default",
+        "options": {
+            "userName": process.env.ODS_USER || "ods_user",
+            "password": process.env.ODS_PASSWORD || "p@ssw0rd"
+        }
+    },
+    "options": {
+        "port": parseInt(process.env.ODS_PORT) || 49412,
+        "trustServerCertificate": process.env.ODS_TRUSTSERVERCERTIFICATE === 'true' || false,
+        "enableArithAbort":  process.env.ODS_ENABLEARITHABORT === 'true' || true,
+        "database": process.env.ODS_DBNAME || "ods-with-amt",
+        "encrypt": process.env.ODS_ENCRYPT === 'true' || false
     }
 }
 /* {
