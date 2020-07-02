283bdc5d602062a7efeb43287f05702210bf7ed9
Kurt Johnson <kurt.johnson@headspring.com>
2020-07-02 09:11:41 -0500

    Move the survey migrations after the ETL column width changes
    DB ETL widened the studentschool columns to accommodate keys. These should be made before they are used in VIEWS.
    
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

--- edfi.fif.database/migrations/20200702095659-create-survey-result-views.js
+++ edfi.fif.database/migrations/20200702095659-create-survey-result-views.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20200623231754-create-survey-result-views-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20200702095659-create-survey-result-views-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20200623231754-create-survey-result-views-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20200702095659-create-survey-result-views-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
